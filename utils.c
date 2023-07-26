d7079cda153ee5c0a804140b97c2baed9d0bbfa9
Adekunleworld <135136858+Adekunleworld@users.noreply.github.com>
2023-07-26 05:47:06 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * is_printable - Evaluates if a char is printable
  * @c: Char to be evaluated.
@@ -10,13 +9,11 @@ int is_printable(char c)
 {
 	if (c >= 32 && c < 127)
 		return (1);
-
 	return (0);
 }
-
 /**
  * append_hexa_code - Append ascci in hexadecimal code to buffer
- * @buffer: Array of chars.
+ * @buffer: Array of characters.
  * @i: Index at which to start appending.
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
@@ -27,16 +24,12 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
-
 	buffer[i++] = '\\';
 	buffer[i++] = 'x';
-
 	buffer[i++] = map_to[ascii_code / 16];
 	buffer[i] = map_to[ascii_code % 16];
-
 	return (3);
 }
-
 /**
  * is_digit - Verifies if a char is a digit
  * @c: Char to be evaluated
